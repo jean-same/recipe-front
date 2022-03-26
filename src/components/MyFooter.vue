38baf4e6dba54b768b7458800ec0d7d2c18197c0
jean-same <jnmauricesam92@gmail.com>
2022-03-26 20:56:29 +0100

    Fix bug footer position

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -1,11 +1,5 @@
 <template>
   <footer class="footer">
-    <div class="waves">
-      <div class="wave" id="wave1"></div>
-      <div class="wave" id="wave2"></div>
-      <div class="wave" id="wave3"></div>
-      <div class="wave" id="wave4"></div>
-    </div>
     <ul class="social-icon">
       <li class="social-icon-item"><a class="social-icon-link" href="https://fr-fr.facebook.com/recettes">
           <b-icon icon="facebook" scale="1"></b-icon>
@@ -46,12 +40,12 @@ export default {
 <style scoped lang="scss">
 
 .footer {
-  position: relative;
+  position:absolute;
+  bottom:0;
   width: 100%;
   background: #3586ff;
   min-height: 40px;
   padding: 15px 30px;
-  margin-top: 50px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -104,62 +98,6 @@ export default {
   margin: 15px 0 10px 0;
   font-size: 1rem;
   font-weight: 300;
-}
-
-.wave {
-  position: absolute;
-  top: -40px;
-  left: 0;
-  width: 100%;
-  height: 40px;
-  background: url("https://i.ibb.co/wQZVxxk/wave.png");
-  background-size: 1000px 100px;
-}
-
-.wave#wave1 {
-  z-index: 1000;
-  opacity: 1;
-  bottom: 0;
-  animation: animateWaves 4s linear infinite;
-}
-
-.wave#wave2 {
-  z-index: 999;
-  opacity: 0.5;
-  bottom: 10px;
-  animation: animate 4s linear infinite !important;
-}
-
-.wave#wave3 {
-  z-index: 1000;
-  opacity: 0.2;
-  bottom: 15px;
-  animation: animateWaves 3s linear infinite;
-}
-
-.wave#wave4 {
-  z-index: 999;
-  opacity: 0.7;
-  bottom: 20px;
-  animation: animate 3s linear infinite;
-}
-
-@keyframes animateWaves {
-  0% {
-    background-position-x: 1000px;
-  }
-  100% {
-    background-positon-x: 0px;
-  }
-}
-
-@keyframes animate {
-  0% {
-    background-position-x: -1000px;
-  }
-  100% {
-    background-positon-x: 0px;
-  }
 }
 
 </style>
